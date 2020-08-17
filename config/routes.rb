d47cbd00cc0e58ae3b5d9fee379dee2bb186ce7e
julien evano <julien.evano@audencia.com>
2020-08-17 17:13:23 +0200

    j'enlève la route castles/reservations inutile selon Nathan

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :castles, only: [:index, :show, :new, :create] do
-    resources :reservations, only: [:new, :create, :index]
+    resources :reservations, only: [:new, :create]
   end
 
   resources :reservations, only: [:index] do
