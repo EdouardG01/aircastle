e8a7a4d6b69642b48dd50f22c649fd312e5e0673
edouard goisbault <edouard.goisbault01@gmail.com>
2020-08-17 16:25:37 +0200

    add devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # config devise
+  config . action_mailer . default_url_options = { hôte : 'localhost' , port : 3000 }
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
