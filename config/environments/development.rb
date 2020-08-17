4e76eba8f576bcf9f0dd52d1ce82577312f4b25b
edouard goisbault <edouard.goisbault01@gmail.com>
2020-08-17 17:53:02 +0200

    finally install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.mailer_sender = 'aircastle@test.com'
 
 
