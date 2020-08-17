2735e7eb75e2d2ede3531a0b1da7246f756a5bdd
julien evano <julien.evano@audencia.com>
2020-08-17 15:53:21 +0200

    routes, correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,18 @@ Rails.application.routes.draw do
   root to: 'castles#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :castles, only [:index, :show, :new, :create]
-
-  resources :castles do
-    resources :reservations, only [:new, :create, :index]
+  resources :castles, only: [:index, :show, :new, :create] do
+    resources :reservations, only: [:new, :create, :index]
   end
 
-  resources :reservations, only [:index]
-
-  resources :reservations do
+  resources :reservations, only: [:index] do
     member do
       get :decline
       get :accept
     end
   end
 
-  resources :owners do
-    resources :reservations, only [:index]
+  namespace :owners do
+    resources :reservations, only: [:index]
   end
 end
