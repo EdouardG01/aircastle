437d3525425f5898931aafadcc1633e90521464f
julien evano <julien.evano@audencia.com>
2020-08-17 15:40:17 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'castles#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :castles, only [:index, :show, :new, :create]
+
+  resources :castles do
+    resources :reservations, only [:new, :create, :index]
+  end
+
+  resources :reservations, only [:index]
+
+  resources :reservations do
+    member do
+      get :decline
+      get :accept
+    end
+  end
+
+  resources :owners do
+    resources :reservations, only [:index]
+  end
 end
