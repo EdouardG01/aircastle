53e18b9939b13b713c03b0f9a6ff17fbae2bad57
Fred Gibert <fred.gib79@gmail.com>
2020-08-17 18:44:43 +0200

    recoded seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-25.times do
-  castle = Castle.new(
-    name:  Faker::Games::Pokemon.name,
-    description: Faker::String.random,
-    price: "#{Faker::Number.decimal(l_digits: 2)}€"
-  )
-  castle.save!
-end
-
 50.times do
   user = User.new(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name ,
-    email: Faker::Internet.email,
-    address: Faker::Address.city
+    email: Faker::Internet.email
   )
   user.save!
 end
+
+25.times do
+  castle = Castle.new(
+    name:  Faker::Games::Pokemon.name,
+    description: Faker::String.random,
+    address: Faker::Address.city,
+    price_per_day: rand(10..20)
+  )
+  random_user = User.order(Arel.sql('RANDOM()')).first
+
+  castle.user = random_user
+
+  castle.save!
+end
+
+
