21e31756c55e9df2f12a5d5a1df0ce1e55cd5739
Julien Victor <victor.julien84@gmail.com>
2020-08-18 16:00:38 +0200

    method new and view create with form-for and permit

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -6,4 +6,24 @@ class CastlesController < ApplicationController
   def show
     @castle = Castle.find(params[:id])
   end
+
+  def new
+    @castle = Castle.new
+  end
+
+  def create
+    @castle = Castle.new(castle_params)
+
+    if @castle.save
+      redirect_to @castle, notice: 'Castle was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def castle_params
+    params.require(:castle).permit(:name, :address, :price_per_day, :description, :castle_image)
+  end
 end
