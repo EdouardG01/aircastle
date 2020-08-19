c55502302484a9d10c85be5a0f804caf8f3ea7e9
Fred Gibert <fred.gib79@gmail.com>
2020-08-19 15:33:19 +0200

    Finished with Mes Chateaux et Mes reservations

--- app/controllers/owner/castles_controller.rb
+++ app/controllers/owner/castles_controller.rb
@@ -1,5 +1,6 @@
 class Owner::CastlesController < ApplicationController
   def index
-    @castles = Castle.where(user: current_user)
+    # @castles = Castle.where(user: current_user)
+    @castles = Castle.all
   end
 end
