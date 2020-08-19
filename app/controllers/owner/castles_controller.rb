46d49b29b02cfc1def7b706222f7f3e9de236ef2
julien evano <julien.evano@audencia.com>
2020-08-19 12:45:29 +0200

    added owner/castles

--- app/controllers/owner/castles_controller.rb
+++ app/controllers/owner/castles_controller.rb
@@ -2,18 +2,4 @@ class Owner::CastlesController < ApplicationController
   def index
     @castles = Castle.where(user: current_user)
   end
-
-  def accept
-    @reservation = Reservation.find(params[:id])
-    @reservation.status = "accepted"
-    @reservation.save!
-    redirect_to reservations_path
-  end
-
-  def decline
-    @reservation = Reservation.find(params[:id])
-    @reservation.status = "declined"
-    @reservation.save!
-    redirect_to reservations_path
-  end
 end
