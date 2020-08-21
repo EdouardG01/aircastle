ca6230dd6ff0c1bd93e656fa94cf606dbbb7a804
Fred Gibert <fred.gib79@gmail.com>
2020-08-21 15:27:28 +0200

    Added picture to map popup

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,6 +21,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      console.log(marker.infoWindow)
 
       // Create a HTML element for your custom marker
       const element = document.createElement('div');
